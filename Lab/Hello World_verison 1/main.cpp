5d1f037186992ee2cece33302125ba8c244a0d22
xinxiang1992 <xiangxin050@gmail.com>
2016-02-22 13:37:52 -0800

    Signed-off-by: xinxiang1992 <xiangxin050@gmail.com>

---
+++ Lab/Hello World_verison 1/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Xin Xiang
+ *
+ * Created on February 17, 2016, 9:45 AM
+ Purpose: Our first program
+*/
+#include <iostream>
+
+using namespace std;
+     //User Libraries
+     //Global Constants
+     //Function Prototypes
+     //Execution Begins Here!
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare variables
+    //Initialize variables
+    //Input values
+    //Map the inputs to the outputs
+    //Output the result 
+    cout<<"Hello World"<<endl;
+    //Exist 
+    return 0;
+}
+
---
+++ Lab/Hello_World_version 2/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Xin Xiang
+ *
+ * Created on February 17, 2016, 9:45 AM
+ Purpose: Our first program
+*/
+#include <iostream>
+
+using namespace std;
+     //User Libraries
+     //Global Constants
+     //Function Prototypes
+     //Execution Begins Here!
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare variables
+    //Initialize variables
+    //Input values
+    //Map the inputs to the outputs
+    //Output the result 
+    std::cout<<"Hello World"<<std::endl;
+    //Exist 
+    return 0;
+}
+
