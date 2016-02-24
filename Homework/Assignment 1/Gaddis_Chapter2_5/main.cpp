c9594fe4ffaad598d93b44e7fca9873d2a5aae6f
xinxiang19920104 <xiangxin050@gmail.com>
2016-02-24 14:53:11 -0800

    Signed-off-by: xinxiang19920104 <xiangxin050@gmail.com>

---
+++ Homework/Assignment 1/Gaddis_Chapter2_5/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Xin Xiang
+ *
+ * Created on February 24, 2016, 2:26 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int a=24, b=28, c=32, d=33, e=37;  //Five values which are given.
+    
+    int sum= a+b+c+d+e;  //Sum of five values.
+    
+    float average= sum/5;  //The average of five values.
+    cout<<"The average of these five values are "<<average<<endl;
+    
+    
+    
+
+    return 0;
+}
+
