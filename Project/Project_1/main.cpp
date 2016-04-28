8c812b69db6d1fc59ea1ff84051adfa3e1eb1b4e
xinxiang1992 <xiangxin050@gmail.com>
2016-04-27 23:32:40 -0700

    Signed-off-by: xinxiang1992 <xiangxin050@gmail.com>

---
+++ Project/Project_1/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: xinxiang
+ *
+ * Created on April 24, 2016, 7:54 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Welcome To Your Journey."<<endl;
+    cout<<endl<<endl;
+    
+    cout<<"*                        * * *                 ***********************                         ***                                     "<<endl;
+    cout<<"*   *                 *         *                         *                                   *   *                                     "<<endl;
+    cout<<"*     *              *            *                       *                                  *     *                                    "<<endl;
+    cout<<"*      *            *              *                      *                                 *       *                                       "<<endl;
+    cout<<"*        *         *                *                     *                                *         *                                         "<<endl;
+    cout<<"*         *       *                  *                    *                               *           *                                              "<<endl;
+    cout<<"*         *      *                    *                   *                              *             *                                        "<<endl;
+    cout<<"*         *     *                      *                  *                             *               *                                       "<<endl;
+    cout<<"*         *     *                      *                  *                            * * * * * * * * * *                                         "<<endl;
+    cout<<"*        *      *                      *                  *                           *                   *                             "<<endl;
+    cout<<"*       *        *                    *                   *                          *                     *                              "<<endl;
+    cout<<"*      *          *                  *                    *                         *                       *                              "<<endl;
+    cout<<"*     *             *               *                     *                        *                         *                              "<<endl;
+    cout<<"*    *                *            *                      *                       *                           *                              "<<endl;
+    cout<<"*   *                   *         *                       *                      *                             *                            "<<endl;
+    cout<<"* *                        * * *                          *                     *                               *                               "<<endl;
+    
+    //Declare Variables;
+    float hp=500.0f,mp=50.0f,exp=0.0;//Health Pool, Mana Pool, Experience Pool
+    cout<<""
+    return 0;
+}
+
